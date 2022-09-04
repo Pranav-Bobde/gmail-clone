d4dbe2cf3ac7531be916278bcd54ccdb86b57afb
Pranav-Bobde <bobdep31@gmail.com>
2022-09-05 01:48:07 +0530

    added emailList, emailRow & mail

---
+++ src/EmailList.jsx
@@ -0,0 +1,70 @@
+import { Checkbox, IconButton } from '@material-ui/core'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
+import RedoIcon from '@material-ui/icons/Redo'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import SettingsIcon from '@material-ui/icons/Settings'
+import InboxIcon from '@material-ui/icons/Inbox'
+import PeopleIcon from '@material-ui/icons/People'
+import LocalOfferIcon from '@material-ui/icons/LocalOffer'
+import Section from "./Section"
+import React from 'react'
+import "./EmailList.css"
+import EmailRow from './EmailRow'
+
+const EmailList = () => {
+  return (
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>   
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>   
+      </div>
+
+      <div className="emailList__sections">
+        <Section Icon={InboxIcon} title="Primary" color="red" selected />
+        <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
+        <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+
+      <div className="emailList__List">
+        <EmailRow 
+        title="Twitch"
+        subject="Hey guys"
+        description="Boku wa saikyo!"
+        time="10pm"
+        />
+        <EmailRow 
+        title="Twitch"
+        subject="Hey guys"
+        description="Boku wa saikyo!"
+        time="10pm"
+        />
+
+      </div>
+    </div>
+  )
+}
+
+export default EmailList
